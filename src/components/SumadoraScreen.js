00014281242dcad73fe1a509f740c0cc18754254
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-02-02 19:45:31 -0400

    TablaScreen finished

--- src/components/SumadoraScreen.js
+++ src/components/SumadoraScreen.js
@@ -1,12 +1,107 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import React from "react";
+import { useState } from "react";
 
 const SumadoraScreen = () => {
-  return (
-    <View>
-      <Text>SumadoraScreen</Text>
-    </View>
-  )
-}
 
-export default SumadoraScreen
+  const [numero1, setNumero1] = useState(0);
+  const [numero2, setNumero2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+  //handlers
+  const handleNumero1 = (numero) => {
+  
+    console.log(numero);
+    setNumero1(Number(numero));
+  };
+
+  const handleNumero2 = (numero) => {
+    setNumero2(Number(numero));
+  }
+
+
+  const sumar = () => {
+    const resultado = numero1 + numero2;
+    setResultado(resultado);
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.subContainer}>
+        <View>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            placeholder="Primer Numero"
+            onChangeText={handleNumero1}
+          />
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            placeholder="Segundo Numero"
+            onChangeText={handleNumero2}
+          />
+        </View>
+        <View style={styles.text}>
+          <Text style={styles.title}>Resultado:</Text>
+          <Text style={styles.text}>{resultado}</Text>
+        </View>
+
+        <Pressable
+          style={{
+            backgroundColor: "#67AAF9",
+            padding: 10,
+            borderRadius: 10,
+            marginTop: 20,
+          
+          }}
+          onPress={sumar}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Sumar</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default SumadoraScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 100,
+    width: "100%",
+    height: "100%",
+  },
+
+  subContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+  },
+
+  input: {
+    width: "100%",
+    height: 40,
+    marginBottom: 20,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  text: {
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
--- src/components/TablaScreen.js
+++ src/components/TablaScreen.js
@@ -1,12 +1,122 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  Keyboard,
+} from "react-native";
+import React from "react";
+import { useState } from "react";
 
 const TablaScreen = () => {
-  return (
-    <View>
-      <Text>TablaScreen</Text>
-    </View>
-  )
-}
+  const [numero, setNumero] = useState(0);
+  const [tabla, setTabla] = useState(0);
 
-export default TablaScreen
+  const handleNumero = (numero) => {
+    setNumero(Number(numero));
+  };
+
+  handleBtn = () => {
+    if (tabla === 0) {
+      setTabla(12);
+      Keyboard.dismiss();
+    } else {
+      setTabla(0);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.subContainer}>
+        <Text style={styles.subTitle}>Tabla de Multiplicar</Text>
+        <TextInput
+          onChangeText={handleNumero}
+          style={styles.input}
+          placeholder="Ingrese un numero"
+          keyboardType="numeric"
+        />
+        {/* <Pressable style={styles.btn} onPress={handleBtn}>
+          <Text style={{ color: "white", textAlign: "center" }}>
+            Generar Tabla
+          </Text>
+        </Pressable> */}
+        {
+          tabla === 0 ? (
+            <Pressable style={styles.btn} onPress={handleBtn}>
+              <Text style={{ color: "white", textAlign: "center" }}>Generar Tabla</Text>
+            </Pressable>
+          ) : (
+            <Pressable style={styles.btnDelete} onPress={() => setTabla(0)}>
+              <Text style={{ color: "white", textAlign: "center" }}>Borrar</Text>
+            </Pressable>
+          )
+        }
+
+        <View style={styles.text}>
+          <Text style={styles.title}>Resultado:</Text>
+          {Array(tabla)
+            .fill(1)
+            .map((item, index) => {
+              return (
+                <Text style={styles.text} key={index}>
+                  {numero} x {index + 1} = {numero * (index + 1)}
+                </Text>
+              );
+            })}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default TablaScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  subContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    marginBottom: 20,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subTitle: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 15,
+    marginTop: 5,
+  },
+  btn: {
+    backgroundColor: "#67AAF9",
+    padding: 10,
+    borderRadius: 10,
+  },
+  btnDelete: {
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 10,
+  },
+});
